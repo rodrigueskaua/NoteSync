a9eea4744d14e9fb697bd33ede6792be8f907649
Kauã F. N. Rodrigues <77804949+rodrigueskaua@users.noreply.github.com>
2023-06-28 10:05:54 -0300

    fix(template): Adicionar referência ao sidebar.js
    Corrige a ausência da referência ao arquivo sidebar.js no template main.blade.php.

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -95,6 +95,7 @@
       </div>
     </div>
     {{-- Script SideBar --}}
+    <script src="js/sidebar.js"></script>
     {{-- Script Bootstrap --}}
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
   </body>
