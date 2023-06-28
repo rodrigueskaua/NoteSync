27e58dc33e9787fa952a3e2f4a0c6b00f686271e
Kauã F. N. Rodrigues <77804949+rodrigueskaua@users.noreply.github.com>
2023-06-28 10:19:30 -0300

    feat(main.css,sidebar.js): Adiciona efeito de blur ao conteúdo
    Adiciona a função e o estilo no arquivo main.css e sidebar.js para aplicar um efeito de blur no conteúdo do site quando o menu estiver em foco.

--- public/js/sidebar.js
+++ public/js/sidebar.js
@@ -1,23 +1,35 @@
-const showMenu = (headerToggle, navbarId) => {
-  const toggleBtn = document.getElementById(headerToggle);
-  const nav = document.getElementById(navbarId);
+const showMenu = (headerToggle, navbarId, contentId) => {
+    const toggleBtn = document.getElementById(headerToggle);
+    const nav = document.getElementById(navbarId);
+    const content = document.querySelector('.main-page');
 
-  if (toggleBtn && nav) {
-    toggleBtn.addEventListener('click', () => {
-      nav.classList.toggle('show-menu');
-      toggleBtn.classList.toggle('bx-x');
-    });
 
-    document.addEventListener('click', (event) => {
-      const target = event.target;
-      const isSideBarOpen = nav.classList.contains('show-menu');
+    if (toggleBtn && nav) {
+        toggleBtn.addEventListener('click', () => {
+            nav.classList.toggle('show-menu');
+            toggleBtn.classList.toggle('bx-x');
+            content.classList.toggle('blur');
+        });
 
-      if (isSideBarOpen && !target.closest('#' + navbarId) && !target.closest('#' + headerToggle)) {
-        nav.classList.remove('show-menu');
-        toggleBtn.classList.remove('bx-x');
-      }
-    });
-  }
+        document.addEventListener('click', (event) => {
+            const target = event.target;
+            const isSideBarOpen = nav.classList.contains('show-menu');
+
+            if (isSideBarOpen && !target.closest('#' + navbarId) && !target.closest('#' + headerToggle)) {
+                nav.classList.remove('show-menu');
+                toggleBtn.classList.remove('bx-x');
+                content.classList.remove('blur');
+            }
+        });
+
+        nav.addEventListener('mouseover', () => {
+            content.classList.add('blur');
+        });
+
+        nav.addEventListener('mouseout', () => {
+            content.classList.remove('blur');
+        });
+    }
 };
 
 showMenu('header-toggle', 'navbar');
@@ -26,8 +38,8 @@ showMenu('header-toggle', 'navbar');
 const linkColor = document.querySelectorAll('.nav-link')
 
 function colorLink() {
-  linkColor.forEach(l => l.classList.remove('active'))
-  this.classList.add('active')
+    linkColor.forEach(l => l.classList.remove('active'))
+    this.classList.add('active')
 }
 
 linkColor.forEach(l => l.addEventListener('click', colorLink))
