e763d794c374f32d354303dadeecb45ba63e3082
Kauã F. N. Rodrigues <77804949+rodrigueskaua@users.noreply.github.com>
2023-06-30 13:58:17 -0300

    Revert "Revert "Adiciona a página de edição de notas e funcionalidades relacionadas""
    This reverts commit c5abcac46d454650cf891d05105fb8b9e4d879fa.

--- routes/web.php
+++ routes/web.php
@@ -6,5 +6,7 @@ use App\Http\Controllers\NoteController;
 Route::get('/', [NoteController::class, 'index'])->name('notes.index');
 
 Route::get('/notes/create', [NoteController::class, 'create'])->name('notes.create');
-
 Route::post('/notes/store', [NoteController::class, 'store'])->name('notes.store');
+
+Route::get('/notes/{id}', [NoteController::class, 'show'])->name('notes.show');
+Route::put('/notes/update/{id}', [NoteController::class, 'update'])->name('notes.update');
