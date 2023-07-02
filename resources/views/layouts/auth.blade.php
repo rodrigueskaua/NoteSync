e16bf763cdc8aa9dbce20974b92404bca3a9e825
Kauã F. N. Rodrigues <77804949+rodrigueskaua@users.noreply.github.com>
2023-07-02 15:26:37 -0300

    feat: Adição do CDN do SweetAlert2 no arquivo template auth.blade.php
    Foi adicionado o link do CDN do SweetAlert2 no arquivo template auth.blade.php para possibilitar a exibição de mensagens de erro na tela de login e registro. O SweetAlert2 é uma biblioteca JavaScript que fornece uma experiência de exibição de alertas e modais mais amigável e personalizável.

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -16,7 +16,9 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,800;1,900&display=swap" rel="stylesheet">
     {{-- Box Icons --}}
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-  </head>
+    {{-- Script CDN SweetAlert2 --}}
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+</head>
   <body>
 
     <div class="container-fluid main-login">
@@ -29,6 +31,8 @@
             </div>
 
             <form action="@yield('form-action')" method="post" class="form">
+                @csrf
+
               @yield('content')
 
             </form>
@@ -42,5 +46,6 @@
       </div>
     {{-- Script CDN Bootstrap --}}
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+
   </body>
 </html>
