6da122709ec661666ffb9791f13cacaa1f9a667f
Kauã F. N. Rodrigues <77804949+rodrigueskaua@users.noreply.github.com>
2023-07-02 20:00:17 -0300

    feat: Atualizações manuais das traduções nos arquivos lang/pt
    Realiza atualizações manuais nas traduções nos arquivos lang/pt para corrigir erros, melhorar a consistência e a qualidade das traduções.

--- lang/pt/passwords.php
+++ lang/pt/passwords.php
@@ -3,9 +3,9 @@
 declare(strict_types=1);
 
 return [
-    'reset'     => 'A palavra-passe foi redefinida!',
-    'sent'      => 'O lembrete para a palavra-passe foi enviado!',
-    'throttled' => 'Por favor aguarde, antes de tentar novamente.',
-    'token'     => 'Este código de recuperação da palavra-passe é inválido.',
-    'user'      => 'Não existe nenhum utilizador com o e-mail indicado.',
+    'reset' => 'A senha foi redefinida com sucesso!',
+    'sent' => 'Um lembrete de senha foi enviado para o seu e-mail!',
+    'throttled' => 'Por favor, aguarde um momento antes de tentar novamente.',
+    'token' => 'Este código de recuperação de senha é inválido.',
+    'user' => 'Não existe nenhum usuário com o e-mail informado.',
 ];
