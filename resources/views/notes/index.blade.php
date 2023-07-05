ac6e761a8bfb3331d0eff0cd3b607260a8f165ab
Kauã F. N. Rodrigues <77804949+rodrigueskaua@users.noreply.github.com>
2023-07-05 17:39:31 -0300

    feat(notes.index, main.css): Atualiza o texto exibido nos cards e substitui botão "leia mais"
    Atualização no arquivo main.css para limitar o texto exibido nos cards a no máximo duas linhas, corrigindo um problema de aparência. Agora, ao clicar no card o usuário é redirecionado para a tela de visualização e edição das notas, substituindo o botão "leia mais".

--- resources/views/notes/index.blade.php
+++ resources/views/notes/index.blade.php
@@ -6,7 +6,7 @@
 @section('title', 'Suas Anotações')
 @section('current-page-name', 'Suas Anotações')
 @section('content')
-<h3>Anotações de {{ explode(" ", auth()->user()->name )[0] }}</h3>
+{{-- <h3>Anotações de {{ explode(" ", auth()->user()->name )[0] }}</h3> --}}
 <div class="notes-container row row-cols-1 row-cols-md-2 justify-content-center">
   @foreach($notes as $note)
   <div class="col">
@@ -14,10 +14,9 @@
       <h2 class="note-title">{{ $note->title }}</h2>
       <p class="note-date">{{ date('d/m/Y', strtotime($note->updated_at)) }}</p>
       <p class="note-content">{{ TextHelper::formatNoteContent($note->content) }}</p>
-      <a href="notes/{{ $note->id }}" class="note-button">Leia mais</a>
+      <a href="notes/{{ $note->id }}" class="note-overlay"></a>
     </div>
   </div>
   @endforeach
 </div>
-
 @endsection
