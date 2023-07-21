d537029347f67ba502cd17cff35b46e6f9cf6979
Kauã F. N. Rodrigues <77804949+rodrigueskaua@users.noreply.github.com>
2023-07-21 16:20:42 -0300

    feat(CKEditor): Eventos para habilitar edição do editor
    Adiciona eventos de clique ao CKEditor e ao input de título, permitindo a edição quando qualquer um deles for clicado. Também foi adicionada uma borda à classe .ck-sticky-panel para melhorar a visualização dos elementos em destaque durante a edição.

--- public/js/CKEditor.js
+++ public/js/CKEditor.js
@@ -2,16 +2,6 @@ ClassicEditor
   .create(document.querySelector('.create'), {
     placeholder: 'Comece a escrever aqui...'
   })
-  .then(editor => {
-
-  })
-  .catch(error => {
-    console.error('Oops, something went wrong!');
-    console.error('Please, report the following error on https://github.com/ckeditor/ckeditor5/issues with the build id and the error stack trace:');
-    console.warn('Build id: 2vx08gcvmrpb-sao95dwge53k');
-    console.error(error);
-  });
-
 
 ClassicEditor
   .create(document.querySelector('.edit'), {
@@ -19,7 +9,7 @@ ClassicEditor
   })
   .then(editor => {
     editor.enableReadOnlyMode('edit');
-
+    const ckEditor = document.querySelector('.ck')
     const toggleModeButton = document.querySelector('#toggleModeButton');
     const titleInput = document.querySelector('#input-title');
 
@@ -32,19 +22,11 @@ ClassicEditor
         toggleModeButton.type = "submit"
         toggleModeButton.classList.add('button-animation');
       }
-
     };
-
+    ckEditor.addEventListener('click', toggleMode);
+    titleInput.addEventListener('click', toggleMode);
     toggleModeButton.addEventListener('click', toggleMode);
   })
-  .catch(error => {
-    console.error('Oops, something went wrong!');
-    console.error('Please, report the following error on https://github.com/ckeditor/ckeditor5/issues with the build id and the error stack trace:');
-    console.warn('Build id: 2vx08gcvmrpb-sao95dwge53k');
-    console.error(error);
-  });
-
-
 
 
 
